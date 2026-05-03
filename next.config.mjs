5a2fc54f5bf08cc6de7f191d2359b206cced1d7a
jubairahmed22 <jubairahmed060@gmail.com>
2026-05-03 12:29:10 +0600

    solve the bug

---
+++ next.config.mjs
@@ -0,0 +1,23 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+      },
+    ],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  compress: true,
+  poweredByHeader: false,
+  reactStrictMode: true,
+  swcMinify: true,
+};
+
+export default nextConfig; // Change from module.exports
